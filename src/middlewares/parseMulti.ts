7d8bf69b6fb335611e4b56ddacc2f2751794b4aa
Nazita <laraianazareno@gmail.com>
2025-11-06 02:43:11 -0300

    fix colors

--- src/middlewares/parseMulti.ts
+++ src/middlewares/parseMulti.ts
@@ -6,9 +6,19 @@ export const parseMultipartPayload: RequestHandler = (req, _res, next) => {
   if (req.is("multipart/form-data") && typeof req.body?.payload === "string") {
     try {
       req.body = JSON.parse(req.body.payload);
-    } catch (e) {
+    } catch {
       return next(new ApiError("payload inválido: no es JSON", 400));
     }
   }
+
+  // ✅ Nuevo: si 'color' viene como string JSON, parsearlo
+  if (typeof req.body?.color === "string") {
+    try {
+      req.body.color = JSON.parse(req.body.color);
+    } catch {
+      // si falla, lo dejamos tal cual para que Zod lo valide
+    }
+  }
+
   next();
 };
--- src/routes/menuRouter.ts
+++ src/routes/menuRouter.ts
@@ -3,13 +3,14 @@ import { validate } from "../middlewares/validate";
 import { createMenuSchema, updateMenuSchema } from "../validations/menu.validation";
 import { getAllMenus, getMenuById, createMenu, updateMenu, deleteMenu } from "../controllers/menuController";
 import { uploadMiddleware } from "../s3-image-module";
+import { parseMultipartPayload } from "../middlewares/parseMulti";
 
 
 const menuRouter = Router();
 
 menuRouter.get("/", getAllMenus);
 menuRouter.get("/:id", getMenuById);
-menuRouter.post("/", uploadMiddleware.any(), validate(createMenuSchema), createMenu);
-menuRouter.put("/:id", uploadMiddleware.any(), validate(updateMenuSchema), updateMenu);
+menuRouter.post("/", uploadMiddleware.any(),parseMultipartPayload, validate(createMenuSchema), createMenu);
+menuRouter.put("/:id", uploadMiddleware.any(),parseMultipartPayload, validate(updateMenuSchema), updateMenu);
 menuRouter.delete("/:id", deleteMenu);
 export default menuRouter;
